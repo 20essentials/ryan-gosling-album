8262d86267b7f9b566e1b75f06f36a38aaa45ef1
DominicDev <s0l0estudi4r@gmail.com>
2026-03-28 13:43:46 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RYAN GOSLING';
+export let nameSongs = [
+	"I’M JUST KEN", /*1*/
+	"IN THE ROOM WHERE YOU SLEEP", /*2*/
+	"LOSE YOUR SOUL", /*3*/
+	"MY BODY'S A ZOMBIE FOR YOU", /*4*/
+	"BURIED IN WATER", /*5*/
+	"DEAD HEARTS", /*6*/
+	"PA PA POWER ", /*7*/
+	"WEREWOLF HEART", /*8*/
+	"YOUNG & TRAGIC", /*9*/
+	"PAPER SHIPS", /*10*/
+	"DEAD MAN'S BONES", /*11*/
+	"FLOWERS GROW OUT OF MY GRAVE", /*12*/
+	"INTRO", /*13*/
+	"NAME IN STONE", /*14*/
+	"THANATOS", /*15*/
+	"WASTELAND", /*16*/
+	"I LOVE YOU", /*17*/
+	"IRON", /*18*/
+	"GOLIATH", /*19*/
+	"IN YOUR LIKENESS", /*20*/
+]
